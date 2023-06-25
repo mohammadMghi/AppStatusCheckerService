608c8b3111b15bde590fc36e20a261ca12bfbcc8
Mohammad <mohammadmghig@gmail.com>
2023-06-25 23:07:03 +0330

    init

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -17,13 +17,13 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
  
-        $schedule->job(new WeeklyJobAppstore())->everyMinute();
-        $schedule->job(new WeeklyJobGooglePlay())->everyMinute();
+        // $schedule->job(new WeeklyJobAppstore())->everyMinute();
+        // $schedule->job(new WeeklyJobGooglePlay())->everyMinute();
  
  
 
-        // $schedule->job(new WeeklyJobAppstore())->weekly()->at('00:00');
-        // $schedule->job(new WeeklyJobGooglePlay())->weekly()->at('00:00');
+        $schedule->job(new WeeklyJobAppstore())->weekly()->at('00:00');
+        $schedule->job(new WeeklyJobGooglePlay())->weekly()->at('00:00');
  
         //and more
     }
